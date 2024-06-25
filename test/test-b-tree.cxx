98abbfd4b075b92df70d3debf0a7283c37acc22a
Huy Nguyen <huy55465@gmail.com>
2024-06-25 02:27:29 -0400

    feat: more explicitly test optional

--- test/test-b-tree.cxx
+++ test/test-b-tree.cxx
@@ -13,9 +13,9 @@ TEST(my_b_tree_test, check_contains) {
     std::unique_ptr<my_b_tree::node<int>> test_tree_node =
         std::make_unique<my_b_tree::node<int>>();
     
-    ASSERT_FALSE(test_tree_node->find(1));
-    ASSERT_FALSE(test_tree_node->find(-1));
-    ASSERT_FALSE(test_tree_node->find(0));
-    ASSERT_FALSE(test_tree_node->find(INT_MAX));
-    ASSERT_FALSE(test_tree_node->find(INT_MIN));
+    ASSERT_FALSE(test_tree_node->find(1).has_value());
+    ASSERT_FALSE(test_tree_node->find(-1).has_value());
+    ASSERT_FALSE(test_tree_node->find(0).has_value());
+    ASSERT_FALSE(test_tree_node->find(INT_MAX).has_value());
+    ASSERT_FALSE(test_tree_node->find(INT_MIN).has_value());
 }
