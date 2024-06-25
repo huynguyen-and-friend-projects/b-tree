d49ce24ef3e18723df8d3336be07a28862bffec0
Huy Nguyen <huy55465@gmail.com>
2024-06-24 21:58:25 -0400

    feat: add find method
    Also move MAX_KEYS and MAX_CHILDREN around I think? But it shouldn't
    affect the code.
    
    commit-by: Huy Nguyen

--- src/b-tree.hxx
+++ src/b-tree.hxx
@@ -8,10 +8,11 @@
 #include <optional>
 
 template <typename T>
-concept num = requires(T val) { std::equality_comparable<T>; };
+concept Num =
+    std::integral<T> || std::floating_point<T> || std::equality_comparable<T>;
 
 namespace my_b_tree {
-template <typename T> class node {
+template <Num T> class node {
   public:
     static constexpr size_t MAX_KEYS = 5;
     static constexpr size_t MAX_CHILDREN = MAX_KEYS + 1;
@@ -26,28 +27,48 @@ template <typename T> class node {
 
     ~node<T>() = default;
 
+    /**
+     * @brief Whether this node has no children.
+     *
+     * @return true if this node has zero children, false otherwise
+     */
     auto is_leaf() -> bool { return this->n_keys == 0; }
+
+    /**
+     * @brief Finds the node containing the specified value, and return that
+     * node if found.
+     *
+     * @param val
+     * @return 
+     */
+    auto find(T val) -> std::optional<node<T>*> {
+        auto ret = find_(this, val);
+        if (ret.has_value()) {
+            return ret.value();
+        }
+        return std::nullopt;
+    }
 
   private:
     size_t n_keys = 0;
     std::array<T, MAX_KEYS> keys{};
     std::array<std::shared_ptr<node<T>>, MAX_CHILDREN> children{};
 
-    auto find_(node<T>* curr, T val) -> std::optional<std::weak_ptr<node<T>>> {
-        int pos = 0;
+    static auto find_(node<T>* curr, T val) -> std::optional<node*> {
+        size_t pos = 0;
 
-        for(; pos < curr->n_keys && val > curr->keys[pos]; ++pos) {
+        for (; pos < curr->n_keys && val > curr->keys[pos]; ++pos) {
         }
 
-        if(pos < curr->n_keys && val == curr->keys[pos]){
+        if (pos < curr->n_keys && val == curr->keys[pos]) {
             return curr;
         }
 
-        if(curr->is_leaf()){
+        if (curr->is_leaf()) {
             return std::nullopt;
         }
 
-        return find_(curr->children[pos], val);
+        return find_(curr->children[pos].get(), val);
     }
 };
 } // namespace my_b_tree
