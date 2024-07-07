e2c611dfb58f15e59c7a335cd2373ffed1e2b385
Huy Nguyen <huy55465@gmail.com>
2024-07-07 15:58:32 -0400

    BREAKING CHANGE: yet another rewrite
    commit-by: Huy Nguyen

--- src/b-tree.hxx
+++ src/b-tree.hxx
@@ -6,79 +6,190 @@
 #ifndef B_TREE_PROTO_H
 #define B_TREE_PROTO_H
 
+#include <array>
+#include <concepts>
+#include <cstddef>
 #include <memory>
+#include <optional>
+
 namespace my_b_tree {
 
-struct node;
+template <typename T>
+concept EqComparable = std::equality_comparable<T>;
 
-class b_tree final {
-  public:
-    explicit b_tree(size_t deg);
-    /**
-     * @brief Moves content of the current b_tree into the specified b_tree.
-     *
-     * Not recommended to use unless it is certain that the variable holding the
-     * current b_tree is no longer being used.
-     *
-     * @param move_to
-     */
-    b_tree(b_tree&& move_to) noexcept;
-    /**
-     * @brief Copies content of the current b_tree into the specified b_tree.
-     *
-     * This operation is VERY expensive, both memory-wise and processor-wise,
-     * especially if the b_tree is large. For most situations, use a reference.
-     * Only use this for back-up.
-     *
-     * @param copy_to
-     */
-    b_tree(const b_tree& copy_to);
-    /**
-     * @brief Moves content of the current b_tree into the specified b_tree.
-     *
-     * Not recommended to use unless it is certain that the variable holding the
-     * current b_tree is no longer being used.
-     *
-     * @param move_to
-     */
-    auto operator=(b_tree&& move_to) noexcept -> b_tree&;
-    /**
-     * @brief Copies content of the current b_tree into the specified b_tree.
-     *
-     * This operation is VERY expensive, both memory-wise and processor-wise,
-     * especially if the b_tree is large. For most situations, use a reference.
-     * Only use this for back-up.
-     *
-     * @param copy_to
-     */
-    auto operator=(const b_tree& copy_to) -> b_tree&;
-    ~b_tree();
+// HACK: forward decl of BTree so that BTreeNode could use it
 
-    /**
-     * @brief Whether this b_tree contains the specified value.
-     *
-     * @param val Specified value
-     */
-    auto contains(int val) -> bool;
+/**
+ * @brief
+ *
+ */
+template <EqComparable T, std::size_t min_deg> class BTree;
 
-    void insert(int val);
-    void remove(int val);
-
-    friend struct node;
-
+template <EqComparable T, std::size_t min_deg> class BTreeNode {
   private:
     /**
-     * @class priv
-     * @brief Private implementation of the b_tree.
+     * @brief The array of keys stored inside this instance of BTreeNode.
+     *
+     * There are always at least 1 more key than there are children.
+     */
+    std::array<T, 2 * min_deg> keys{0};
+    /**
+     * @brief The array of children pointers from this instance of BTreeNode.
+     *
+     * There are always at least 1 more key than there are children.
+     * If the BTreeNode is leaf, there is no children.
+     */
+    std::array<std::unique_ptr<BTreeNode>, 2 * min_deg - 1> children{0};
+
+    /**
+     * @brief Parent of this instance of BTreeNode. If this instance is the tree
+     * root, parent is nullptr parent is nullptr (or in this case,
+     * !parent.has_value()).
      *
      */
-    class priv;
+    std::optional<BTreeNode*> parent;
     /**
-     * @brief Private implementation of the b_tree.
+     * @brief Number of keys currently stored by this instance of BTreeNode
      */
-    std::unique_ptr<priv> pimpl_;
+    std::size_t n_keys{0};
+    /**
+     * @brief The index of this instance of BTreeNode's pointer stored inside
+     * its parent's children pointer array.
+     */
+    std::size_t index;
+    /**
+     * @brief Number of children currently stored by this instance of BTreeNode
+     */
+    bool leaf{true};
+
+    /**
+     * @brief Finds an instance of BTreeNode that is either this instance or a
+     * descendant that contains the value specified.
+     *
+     * @param val
+     * @return the pointer to the BTreeNode containing
+     */
+    [[nodiscard]] auto find(T val) const -> std::optional<const BTreeNode*> {
+        std::size_t pos = n_keys / 2;
+        std::size_t lptr = 0;
+        std::size_t rptr = n_keys;
+
+        // binary search with a flavor of trauma
+        while (lptr <= rptr) {
+            if (val == keys[pos]) {
+                return this;
+            }
+            if (val > keys[pos]) {
+                if (lptr == ULONG_MAX) [[unlikely]] {
+                    // INFO: at this point, pos = ULONG_MAX
+                    break;
+                }
+                lptr = pos + 1;
+            } else {
+                if (rptr == 0) [[unlikely]] {
+                    // INFO: at this point, pos = 0
+                    break;
+                }
+                rptr = pos - 1;
+            }
+            // INFO: this is to prevent the extreme case of lptr and rptr being
+            // too big that they overflow size_t.
+            // HACK: this could be a left bit-shift, which technically can be
+            // faster. But I find division more intent-clear.
+            pos = lptr / 2 + rptr / 2;
+        }
+        if (leaf) {
+            return std::nullopt;
+        }
+        return children[pos].get()->find(val);
+    }
+
+    /**
+     * @brief Splits the current node into two, if it's full.
+     *
+     * If the current node is not full, this function simply does nothing.
+     *
+     * @return true if the current node is split, false otherwise.
+     */
+    auto split() -> bool {
+        if (n_keys < keys.size()) {
+            return false;
+        }
+
+        if (!parent.has_value()) {
+            // TODO: finish the split method for root node
+            return true;
+        }
+        return true;
+    }
+
+    friend class BTree<T, min_deg>;
+
+  public:
+    /* boilerplate */
+
+    explicit BTreeNode(BTreeNode* parent, std::size_t idx) : index(idx) {
+        if (parent == nullptr) {
+            this->parent = std::nullopt;
+        }
+        this->parent = parent;
+    };
+
+    BTreeNode(BTreeNode&&) = default;
+    BTreeNode(const BTreeNode&) = default;
+    auto operator=(BTreeNode&&) -> BTreeNode& = default;
+    auto operator=(const BTreeNode&) -> BTreeNode& = default;
+    ~BTreeNode() = default;
+
+    /**
+     * @brief Returns the number of keys in this instance of BTreeNode
+     *
+     * @return The number of keys in this instance of BTreeNode
+     */
+    [[nodiscard]] auto key_count() const -> std::size_t { return n_keys; }
+    /**
+     * @brief Returns the number of children in this instance of BTreeNode
+     *
+     * @return The number of children in this instance of BTreeNode
+     */
+    [[nodiscard]] auto children_count() const -> std::size_t {
+        if (leaf) {
+            return 0;
+        }
+        return n_keys - 1;
+    }
+    /**
+     * @brief Returns whether this instance of BTreeNode is a leaf node
+     *
+     * @return Whether this instance of BTreeNode is a leaf node
+     */
+    [[nodiscard]] auto is_leaf() const -> bool { return leaf; }
+};
+
+template <EqComparable T, std::size_t min_deg> class BTree {
+  private:
+    /**
+     * @brief This instance of BTree's root. At the beginning, the root has no
+     * key
+     */
+    BTreeNode<T, min_deg> root{nullptr, 0};
+
+  public:
+    BTree() = default;
+    BTree(BTree&&) = default;
+    BTree(const BTree&) = default;
+    auto operator=(BTree&&) -> BTree& = default;
+    auto operator=(const BTree&) -> BTree& = default;
+    ~BTree() = default;
+
+    auto get_root() -> BTreeNode<T, min_deg>& { return root; }
+
+    [[nodiscard]] auto
+    find(T val) const -> std::optional<const BTreeNode<T, min_deg>*> {
+        return root.find(val);
+    }
 };
 
 } // namespace my_b_tree
 
-#endif // !B_TREE_PROTO_H
+#endif //! B_TREE_PROTO_H
--- test/test-b-tree.cxx
+++ test/test-b-tree.cxx
@@ -1,17 +1,15 @@
-/**
- * @file test-b-tree.cxx
- * @brief Unit testing of b_tree
- */
-
 #include "b-tree.hxx"
 #include <gtest/gtest.h>
 
 namespace bt = my_b_tree;
-TEST(the_tree, contain) {
-    bt::b_tree test_tree{3};
-    ASSERT_FALSE(test_tree.contains(1));
-    ASSERT_FALSE(test_tree.contains(-1));
-    ASSERT_FALSE(test_tree.contains(0));
-    ASSERT_FALSE(test_tree.contains(INT_MAX));
-    ASSERT_FALSE(test_tree.contains(INT_MIN));
+
+static constexpr std::size_t TEST_TREE_MIN_DEG = 2;
+
+TEST(b_tree, obvi) {
+    bt::BTree<int, TEST_TREE_MIN_DEG> test_tree{};
+    ASSERT_TRUE(test_tree.get_root().is_leaf());
+    ASSERT_EQ(test_tree.get_root().key_count(), 0);
+    ASSERT_EQ(test_tree.get_root().children_count(), 0);
+    ASSERT_FALSE(test_tree.find(69).has_value());
+    test_tree.find(420).has_value(); // NOLINT(*magic-numbers)
 }
