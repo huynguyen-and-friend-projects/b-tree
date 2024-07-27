9270f107d3a0305a2f52ef5f376bf8c21207058a
Huy Nguyen <huy55465@gmail.com>
2024-07-27 11:05:07 -0400

    change variable names

--- Options.cmake
+++ Options.cmake
@@ -10,34 +10,35 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
                  PROPERTY STRINGS "Debug;Release;MinSizeRel;RelWithDebInfo")
 endif()
 
-option(USE_CCACHE "Use ccache" OFF)
-option(USE_LLD "Use lld instead of ld for linking" OFF)
-option(USE_LIBCXX "Use libcxx instead of stdlibcxx" OFF)
-option(ENABLE_OPTIMIZATION
+option(btree_USE_CCACHE "Use ccache" OFF)
+option(btree_USE_LLD "Use lld instead of ld for linking" OFF)
+option(btree_USE_LIBCXX "Use libcxx instead of stdlibcxx" OFF)
+option(btree_btree_ENABLE_OPTIMIZATION
        "Add some optimization flags. Maybe useful only when build type is Debug"
        ON)
-option(ENABLE_PCH "Enable precompiled header" ON)
-option(ENABLE_TESTING "Enable Google test" ON)
-option(ENABLE_WARNING "Enable compiler warnings" ON)
-option(WARNING_AS_ERROR "Change compiler warnings to errors" ON)
-option(ENABLE_ASAN "Compile with AddressSanitizer" OFF)
-option(ENABLE_UBSAN "Compile with UndefinedBehaviorSanitizer" OFF)
-option(ENABLE_MSAN "Compile with MemorySanitizer" OFF)
-option(ENABLE_COVERAGE "Compile with coverage flag" OFF)
-option(ENABLE_FUZZ "Enable fuzz testing. Currently only working with clang" OFF)
+option(btree_ENABLE_PCH "Enable precompiled header" ON)
+option(btree_ENABLE_TESTING "Enable Google test" ON)
+option(btree_ENABLE_WARNING "Enable compiler warnings" ON)
+option(btree_WARNING_AS_ERROR "Change compiler warnings to errors" ON)
+option(btree_ENABLE_ASAN "Compile with AddressSanitizer" OFF)
+option(btree_ENABLE_UBSAN "Compile with UndefinedBehaviorSanitizer" OFF)
+option(btree_ENABLE_MSAN "Compile with MemorySanitizer" OFF)
+option(btree_ENABLE_COVERAGE "Compile with coverage flag" OFF)
+option(btree_ENABLE_FUZZ
+       "Enable fuzz testing. Currently only working with clang" OFF)
 option(
-    BTREE_CLANGD_COMPAT
+    btree_CLANGD_COMPAT
     "Enable supposedly unnecessary compile flags for the b-tree target, mainly so that clangd doesn't throw a bunch of false positives"
     OFF)
 
 if(NOT "${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-    mark_as_advanced(FORCE ENABLE_OPTIMIZATION)
+    mark_as_advanced(FORCE btree_ENABLE_OPTIMIZATION)
 else()
-    mark_as_advanced(CLEAR ENABLE_OPTIMIZATION)
+    mark_as_advanced(CLEAR btree_ENABLE_OPTIMIZATION)
 endif()
 
 # configure accordingly to options
-if(ENABLE_CCACHE)
+if(btree_ENABLE_CCACHE)
     find_program(CCACHE ccache)
     if(NOT CCACHE)
         message("Cannot find ccache")
@@ -48,12 +49,12 @@ if(ENABLE_CCACHE)
     endif()
 endif()
 
-if(ENABLE_OPTIMIZATION)
+if(btree_ENABLE_OPTIMIZATION)
     if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
         if(MSVC)
-            target_compile_options(b-tree-compile-opts INTERFACE "/Zo")
+            target_compile_options(btree_compile_opts INTERFACE "/Zo")
         else(MSVC)
-            target_compile_options(b-tree-compile-opts INTERFACE "-Og")
+            target_compile_options(btree_compile_opts INTERFACE "-Og")
         endif()
     endif()
 else()
@@ -66,14 +67,14 @@ else()
             WARNING "Turning off optimization is only allowed in debug build.")
     else()
         if(MSVC)
-            target_compile_options(b-tree-compile-opts INTERFACE "/Od")
+            target_compile_options(btree_compile_opts INTERFACE "/Od")
         else(MSVC)
-            target_compile_options(b-tree-compile-opts INTERFACE "-O0")
+            target_compile_options(btree_compile_opts INTERFACE "-O0")
         endif()
     endif()
 endif()
 
-if(USE_LLD)
+if(btree_USE_LLD)
     find_program(LLD lld REQUIRED)
     if(NOT LLD)
         message(
@@ -84,96 +85,95 @@ if(USE_LLD)
     set(CMAKE_LINKER lld)
 endif()
 
-if(USE_LIBCXX)
+if(btree_USE_LIBCXX)
     if(NOT MSVC)
-        target_compile_options(b-tree-compile-opts INTERFACE "-stdlib=libc++")
-        target_link_options(b-tree-compile-opts INTERFACE
+        target_compile_options(btree_compile_opts INTERFACE "-stdlib=libc++")
+        target_link_options(btree_compile_opts INTERFACE
                             "-stdlib=libc++;-lc++abi")
     endif()
 endif()
 
-if(ENABLE_TESTING)
+if(btree_ENABLE_TESTING)
     add_subdirectory(unittest)
 endif()
-if(ENABLE_WARNING)
+if(btree_ENABLE_WARNING)
     if(MSVC)
-        target_compile_options(b-tree-compile-opts INTERFACE "/W4")
+        target_compile_options(btree_compile_opts INTERFACE "/W4")
     else(MSVC)
         target_compile_options(
-            b-tree-compile-opts
+            btree_compile_opts
             INTERFACE
                 "-Wall;-Wextra;-Wformat=2;-fdiagnostics-color=always;-Wshadow;-Wconversion"
         )
     endif()
 endif()
 
-if(WARNING_AS_ERROR)
+if(btree_WARNING_AS_ERROR)
     if(MSVC)
-        target_compile_options(b-tree-compile-opts INTERFACE "/WX")
+        target_compile_options(btree_compile_opts INTERFACE "/WX")
     else(MSVC)
-        target_compile_options(b-tree-compile-opts INTERFACE "-Werror")
+        target_compile_options(btree_compile_opts INTERFACE "-Werror")
     endif()
 endif()
 
-if(ENABLE_ASAN)
+if(btree_ENABLE_ASAN)
     if(MSVC)
         target_compile_options(
-            b-tree-compile-opts
+            btree_compile_opts
             INTERFACE
                 "/fsanitize=address;/D_DISABLE_VECTOR_ANNOTATION;/D_DISABLE_STRING_ANNOTATION"
         )
     else(MSVC)
         target_compile_options(
-            b-tree-compile-opts
+            btree_compile_opts
             INTERFACE "-fsanitize=address;-fno-omit-frame-pointer")
-        target_link_options(b-tree-compile-opts INTERFACE "-fsanitize=address")
+        target_link_options(btree_compile_opts INTERFACE "-fsanitize=address")
     endif()
 endif()
 
-if(ENABLE_UBSAN)
+if(btree_ENABLE_UBSAN)
     if(MSVC)
         message(
             "We don't know if there's UBSan support on MSVC :(. Currently disabling it."
         )
     else(MSVC)
-        target_compile_options(b-tree-compile-opts
+        target_compile_options(btree_compile_opts
                                INTERFACE "-fsanitize=undefined")
-        target_link_options(b-tree-compile-opts INTERFACE
-                            "-fsanitize=undefined")
+        target_link_options(btree_compile_opts INTERFACE "-fsanitize=undefined")
     endif()
 endif()
 
-if(ENABLE_MSAN)
+if(btree_ENABLE_MSAN)
     if("${CMAKE_CXX_COMPILER_FRONTEND_VARIANT}" STREQUAL "MSVC")
         message(
             "We don't know if there's MSan support on MSVC :(. Currently disabling it."
         )
     else()
         target_compile_options(
-            b-tree-compile-opts
+            btree_compile_opts
             INTERFACE
                 "-fsanitize=memory;-fno-omit-frame-pointer;-fno-optimize-sibling-calls"
         )
-        target_link_options(b-tree-compile-opts INTERFACE "-fsanitize=memory")
+        target_link_options(btree_compile_opts INTERFACE "-fsanitize=memory")
     endif()
 endif()
 
-if(ENABLE_COVERAGE)
+if(btree_ENABLE_COVERAGE)
     if(MSVC)
         target_compile_options("/fsanitize-coverage=edge")
     elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        target_compile_options(b-tree-compile-opts INTERFACE "--coverage")
-        target_link_options(b-tree-compile-opts INTERFACE "--coverage")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        target_compile_options(btree_compile_opts INTERFACE "--coverage")
+        target_link_options(btree_compile_opts INTERFACE "--coverage")
+    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL ".*Clang")
         target_compile_options(
-            b-tree-compile-opts
+            btree_compile_opts
             INTERFACE "-fprofile-instr-generate;-fcoverage-mapping")
-        target_link_options(b-tree-compile-opts INTERFACE
+        target_link_options(btree_compile_opts INTERFACE
                             "-fprofile-instr-generate;-fcoverage-mapping")
     endif()
 endif()
 
-if(ENABLE_FUZZ)
+if(btree_ENABLE_FUZZ)
     if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
         message(
             "Note that gcc/g++ won't work with libFuzzer. This is a LLVM-only tool."
