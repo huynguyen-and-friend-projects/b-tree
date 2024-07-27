b373316bf6092d0ef188a81d421b16bd9333f571
Huy Nguyen <huy55465@gmail.com>
2024-07-27 10:57:08 -0400

    tweak fuzz test a bit

--- fuzztest/fuzz-test.cxx
+++ fuzztest/fuzz-test.cxx
@@ -2,13 +2,11 @@
 #include <cstddef>
 #include <cstdint>
 #include <exception>
-#include <mutex>
+#include <iterator>
 #include <vector>
 
 // NOLINTBEGIN
 namespace bt = my_b_tree;
-
-static std::mutex guard;
 
 static bt::BTree<int, 4> test_tree{};
 
@@ -16,30 +14,24 @@ static std::vector<int> data_vec{};
 
 namespace {
 void btree_remove() {
-    for(const auto& key : data_vec) {
+    for (const int key : data_vec) {
         test_tree.remove(key);
     }
+    data_vec.clear();
 }
 
 void btree_insert(const uint8_t* data, std::size_t size) {
-    const std::lock_guard<std::mutex> lock(guard);
-    for (std::size_t offset = 0; offset < size; offset += sizeof(int)) {
-        if (offset + 4 >= size - 1) {
-            btree_remove();
-            return;
-        }
+    for (std::size_t offset = 0; offset < size; ++offset) {
         // HACK: int assumed to be 4 bytes here
         const int key =
-            static_cast<int>((data[offset] << 24) | (data[offset + 1] << 16) |
-                             (data[offset + 2] << 8) | (data[offset + 3]));
-        if (!test_tree.contains(key)) {
-            data_vec.push_back(key);
-            test_tree.insert(key);
-        }
+            static_cast<int>(*std::next(data, static_cast<long>(offset)));
+        data_vec.push_back(key);
+        test_tree.insert(key);
         if (!test_tree.contains(key)) {
             std::terminate();
         }
     }
+    btree_remove();
 }
 
 } // namespace
